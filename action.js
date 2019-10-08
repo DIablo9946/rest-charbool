98a4474b8f484cbe1fc157d0dfac7686c4d9afa6
Diablo9946 <diabloch9946@gmail.com>
2019-10-08 18:16:23 +0200

    Tried to do the donought.

--- action.js
+++ action.js
@@ -1,7 +1,7 @@
 function init(){
 
 printLineGraph();
-
+printDonaughtGraph()
 };
 
 $(document).ready(init);
@@ -23,7 +23,7 @@ function getMonthProfit(data){
     for(var i=0; i< data.length; i++){
 
       var d = data[i];
-      var date = d.date;
+      var date = d.date; //come mai .date???
       var amount = Number(d.amount);
 
       var month = moment(date, "DD/MM/YYYY").month();
@@ -40,7 +40,7 @@ function getMonthProfit(data){
 function printLineGraph(){
 
   $.ajax({
-    url : "http://157.230.17.132:3000/todos",
+    url : "http://157.230.17.132:4003/sales",
     method : "GET",
     success: function(data){
       console.log(data);
@@ -54,6 +54,68 @@ function printLineGraph(){
                 datasets: [{
                     label: '# of Votes',
                     data: monthProfit,
+                    backgroundColor: 	['rgba(255, 0, 0, 0.3)'],
+                    borderColor: ['rgba(0, 0, 255, 0.6)'],
+                }]
+            },
+        });
+    },
+    error: function(){
+
+    }
+  });
+};
+
+function getSalesmanAmount(data){
+
+
+var agenti = {
+  nome : [],
+  somma : []
+};
+
+for (var i=0; i< data.length; i++){
+
+  var d = data[i];
+
+  var nome = d.salesman;
+
+  if(!agenti.nome.includes(nome)){
+    agenti.nome.push(nome);
+    agenti.somma.push(0)
+  };
+
+  var amount = d.amount;
+
+  for (var x=0; x < agenti.nome.length; x++){
+    if (nome == agenti.nome[x]){
+      agenti.somma[x] += amount;
+    }
+  }
+
+};
+  return agenti;
+};
+
+function printDonaughtGraph(){
+
+  $.ajax({
+    url : "http://157.230.17.132:4003/sales",
+    method : "GET",
+    success: function(data){
+      console.log(data);
+
+        getSalesmanAmount(data);
+        var ctx = document.getElementById('myChart').getContext('2d');
+        var myPieChart  = new Chart(ctx, {
+            type: 'pie',
+            data: {
+                labels: agenti,
+                datasets: [{
+                    label: '# of Votes',
+                    data: somma,
+                    backgroundColor: 	['rgba(255, 0, 0, 0.3)'],
+                    borderColor: ['rgba(0, 0, 255, 0.6)'],
                 }]
             },
         });
